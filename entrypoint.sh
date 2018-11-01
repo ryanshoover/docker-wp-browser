3645dcd2e52c817212b548146e66e8493930ddb2
Ryan Hoover <ryanshoover@gmail.com>
2018-11-01 12:44:14 -0500

    Improvements to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,26 +4,27 @@
 service apache2 start
 
 # Link codeception config if not yet linked
-if ! $( ls codeception.dist.yml ); then
+if [ ! -e codeception.dist.yml ]; then
 	ln -s ../config/codeception.dist.yml ./codeception.dist.yml
 fi
 
-# Download WordPress if not yet downloaded
-if ! $( wp core version --allow-root ); then
-	wp core download --skip-content --quiet --allow-root
-fi
+# Download WordPress
+wp core download \
+	--skip-content \
+	--quiet \
+	--allow-root
 
-# Config WordPress if not yet config'd
-if ! $( wp config path --allow-root ); then
-	wp config create \
-		--dbname="$DB_NAME" \
-		--dbuser="$DB_USER" \
-		--dbpass="$DB_PASSWORD" \
-		--dbhost="$DB_HOST" \
-		--dbprefix="$WP_TABLE_PREFIX" \
-		--skip-check \
-		--allow-root
-fi
+# Config WordPress
+wp config create \
+	--dbname="$DB_NAME" \
+	--dbuser="$DB_USER" \
+	--dbpass="$DB_PASSWORD" \
+	--dbhost="$DB_HOST" \
+	--dbprefix="$WP_TABLE_PREFIX" \
+	--skip-check \
+	--quiet
+	--allow-root
+
 
 # Install WP if not yet installed
 if ! $( wp core is-installed --allow-root ); then
